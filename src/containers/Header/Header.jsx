9a7c3afc967d960c90e94b4c7e74151da397eaa9
Durin TASONDOCK <temgoua484@gmail.com>
2022-12-20 21:09:39 +0100

    header and footer details

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -12,23 +12,29 @@ const Header = () => {
       <div className='logo'><img src="favicon.png" alt="scoops-pharmacy" data-aos="fade-right" /><span className='logo--text'>SCOOPS <span className='black'>PHARMATY5</span></span></div>
       <nav className='large-nav'>
         <ul>
-          <li><a className='nav-link' href="#mission">Mission</a></li>
-          <li><a className='nav-link' href="#specs">Spécialités</a></li>
-          <li><a className='nav-link' href="#prods">Produits</a></li>
-          <li><a className='nav-link' href="#tests">Témoignages</a></li>
+          <li className='nav-link'><div className='link-dot'/><a href="#mission">Mission</a></li>
+          <li className='nav-link'><div className='link-dot'/><a href="#specs">Spécialités</a></li>
+          <li className='nav-link'><div className='link-dot'/><a href="#prods">Produits</a></li>
+          <li className='nav-link'><div className='link-dot'/><a href="#tests">Témoignages</a></li>
         </ul>
       </nav>
       <div className='header-buttons'>
-        <span className='contact-btn whatsapp'><a href={contacts.whatsapp} target="_blank" rel="noopener noreferrer"><BsWhatsapp /><span className='contact-btn__text'>whatsapp</span></a> </span>
-        <span className='contact-btn telephone'><BiPhone /> <span className='contact-btn__text'>{contacts.phone}</span></span>
+        <a className='contact-btn whatsapp' href={contacts.whatsapp} target="_blank" rel="noopener noreferrer"><BsWhatsapp /><span className='contact-btn__text'>whatsapp</span></a>
+        <span className='contact-btn telephone'>
+          <BiPhone /> 
+          <span className='contact-btn__text'>{contacts.phone}</span>
+        </span>
         <div className='toggle-menu'>
           <div className='nav-button'><BiMenu data-aos="fade-left" /></div>
-          <nav className='small-nav'>
-            <a href="#mission">Mission</a>
-            <a href="#specs">Spécialités</a>
-            <a href="#prods">Produits fars</a>
-            <a href="#tests">Témoignages</a>
-          </nav>
+          <div className='small-nav'>
+            <div className='space' />
+            <nav>
+              <a href="#mission">Mission</a>
+              <a href="#specs">Spécialités</a>
+              <a href="#prods">Produits fars</a>
+              <a href="#tests">Témoignages</a>
+            </nav>
+          </div>
         </div>
       </div>
     </header>
