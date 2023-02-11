4263bed8ed940e0c02e18243877043b86e7d0b32
Durin TASONDOCK <temgoua484@gmail.com>
2023-02-11 15:11:15 +0100

    Quick Commit : probably not important change

--- src/utils/constants/contacts.js
+++ src/utils/constants/contacts.js
@@ -1,7 +1,7 @@
 const contacts = {
   phone: "697 952 344",
   email: "ndjifackernest@gmail.com",
-  whatsapp: "https://wa.me/c/237697952344",
+  whatsapp: "https://wa.me/message/HIVWJAK64JX2D1",
   location: "Yaoundé - Mbankolo"
 }
 
